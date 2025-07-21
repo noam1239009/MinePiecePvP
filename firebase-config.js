9e102544e82e57860f1c7a1ca34ab380ad161bb3
noam1239009 <noamshvetz72@gmail.com>
2025-07-21 19:39:56 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,9 @@
 // firebase-config.js
-// 1) Include your config from the console
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getStorage, ref as sRef, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBaL_3t1OcX08_WAe1pxvhcpUeTZ9ssZ2U",
   authDomain: "one-piece-pvp.firebaseapp.com",
@@ -10,8 +14,11 @@ const firebaseConfig = {
   appId: "1:859496504075:web:34f6fa4bfd0d9a8a8ba00e"
 };
 
-// 2) Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+const storage = getStorage(app);
 
-// 3) Export the database reference
-const db = firebase.database();
+// Export for use in other files
+export { auth, db, storage, ref, set, get, child, sRef, uploadBytesResumable };
